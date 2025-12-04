863e74e980bd916b1a8a86b0b8bc1fd1715f09cf
Viktoria Kalinina <viktoriakalinina@orderberry.de>
2025-12-04 06:28:48 +0100

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,27 @@
 from rest_framework import serializers
 
+from car.models import Car
+
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    model = serializers.CharField(required=True, max_length=64)
+    manufacturer = serializers.CharField(required=True, max_length=64)
+    horse_power = serializers.IntegerField(
+        required=True, min_value=1, max_value=1914
+    )
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        allow_null=True
+    )
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import json
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    json_str = json.dumps(serializer.data)
+    return json_str.encode("utf-8")
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json_data: bytes) -> Car:
+    data = json.loads(json_data.decode("utf-8"))
+    serializer = CarSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    return serializer.save()
